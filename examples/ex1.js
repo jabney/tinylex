11d63816bb84fcbdeda69e96fa2b8d2ee406e923
James Abney <james.abney@gmail.com>
2018-01-02 10:06:45 -0800

    Add to project

---
+++ examples/ex1.js
@@ -0,0 +1,55 @@
+const TinyLex = require('../index')
+
+const KEYWORDS = [
+  'summon', 'forge', 'craft', 'wield', 'if', 'while', 'true', 'false', 'nil'
+]
+
+const KEYWORD = new RegExp(`^(?:${KEYWORDS.join('|')})`)
+const COMMENT = /^\s*(#.*)\n/
+const IDENTIFIER = /^[a-z]\w*/
+const NUMBER = /^(?:\+|-)?(?:\.)?\d+\.?(?:\d+)?/
+const STRING_SINGLE = /^'([^']*)'/
+const STRING_DOUBLE = /^"([^"]*)"/
+const LOGICAL = /^(?:\|\||&&|==|!=|<=|>=)/
+const WHITESPACE = /^\s/
+
+const rules = [
+  [COMMENT, 'COMMENT'],
+  [KEYWORD, 0],
+  [IDENTIFIER, 'IDENTIFIER'],
+  [NUMBER, 'NUMBER'],
+  [LOGICAL, 0],
+  [STRING_SINGLE, 'STRING'],
+  [STRING_DOUBLE, 'STRING'],
+  [WHITESPACE]
+]
+
+const code = `
+#
+# Darklord source
+#
+summon "messenger"
+
+forge harken(msg) {
+  messenger(msg)
+}
+
+craft lieutenants = 12
+craft message: "I have " + leutenants + " servants"
+
+harken.wield(message)
+`
+
+const lexer = new TinyLex(code, rules, { throwOnMismatch: false })
+
+for (let token of lexer) {
+  console.log(token)
+}
+
+while(!lexer.done()) {
+  console.log(lexer.lex())
+}
+
+console.log([...lexer])
+
+console.log(lexer.tokenize())
